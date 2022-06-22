8abb03b8e9d6923f8e96dc4cf42102458c3f386c
sjunh812 <sjunh812@gmail.com>
2022-06-22 17:47:02 +0900

    2022-06-22

--- 그리디/BaekjoonGreedy11000.java
+++ 그리디/BaekjoonGreedy11000.java
@@ -1,66 +1,57 @@
 package sjh;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class BaekjoonGreedy11000 {
-
-	LinkedList<Lecture> lectures = new LinkedList<>();
+	
+	ArrayList<Lecture> lectures = new ArrayList<>();
 	
 	public class Lecture implements Comparable<Lecture> {
 		
-		long start;
-		long end;
+		int start;
+		int end;
 		
-		public Lecture(long start, long end) {
+		public Lecture(int start, int end) {
 			this.start = start;
 			this.end = end;
 		}
-		
+
 		@Override
 		public int compareTo(Lecture o) {
 			if(this.start > o.start) return 1;
 			else if(this.start < o.start) return -1;
-				
+			else {
+				if(this.end > o.end) return 1;
+				else if(this.end < o.end) return -1;
+			}
 			return 0;
 		}
 		
 	}
 	
-    public void solution() throws Exception {  
+    public void solution() throws Exception {  	
     	input();
     	Collections.sort(lectures);
     	
-    	int result = 0;
-    	Lecture compare = null;
+    	PriorityQueue<Integer> pq = new PriorityQueue<>();
     	
-    	while(!lectures.isEmpty()) {
-    		LinkedList<Lecture> temp = new LinkedList<>();
-    		int size = lectures.size();
-    		
-    		for(int i = 0; i < size; i++) {
-				Lecture l = lectures.poll();
-				
-				if(i == 0) {
-					compare = l;
-				} else {
-					if(compare.end <= l.start) {
-						compare = l;
-					} else {
-						temp.offer(compare);
-					}
-				}
-			}
-
-			lectures = temp;
-    		result++;
+    	for(Lecture l : lectures) {
+    		if(pq.isEmpty()) pq.offer(l.end);
+    		else {
+    			if(pq.peek() <= l.start) {
+    				pq.poll();
+    				pq.offer(l.end);
+    			} else {
+    				pq.offer(l.end);
+    			}
+    		}
     	}
     	
-    	System.out.println(result);
-    	
+    	System.out.println(pq.size());
     }
     
     public static void main(String[] args) throws Exception {
@@ -69,12 +60,12 @@ public class BaekjoonGreedy11000 {
 
     public void input() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());	
+        int n = Integer.parseInt(br.readLine());
         
         for(int i = 0; i < n; i++) {
         	StringTokenizer st = new StringTokenizer(br.readLine());
-        	long start = Long.parseLong(st.nextToken());
-        	long end = Long.parseLong(st.nextToken());
+        	int start = Integer.parseInt(st.nextToken());
+        	int end = Integer.parseInt(st.nextToken());
         	lectures.add(new Lecture(start, end));
         }
     }
